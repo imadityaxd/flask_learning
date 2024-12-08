8b21f73cd907ea1f3ad934a0826e2244eacf629e
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-12-08 22:48:26 +0530

    beginning flask

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return 'Hello,World!'
+    return render_template('index.html')
+    #return 'Hello,World!'
+
+@app.route("/products")
+def products():
+    return 'Products of this website'
 
 if __name__ == "__main__":
     app.run(debug  =True)
